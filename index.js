c777680f4901df41e893a1de7575453892263cb9
KUMARI NEHA <92735311+200221192@users.noreply.github.com>
2023-02-16 15:56:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+var options = {
+    strings: ['I AM NEHA.'],
+    typeSpeed: 100 ,
+    backspeed:100,
+    loop: true
+  };
+var typed = new Typed('.typing', options);
+var choose = {
+  strings: ['MY PROJECTS'],
+  typeSpeed: 100 ,
+  backspeed:100,
+  loop: true
+};
+var typed = new Typed('.typed',choose);
+var variety = {
+  strings: ['MY CONTACT'],
+  typeSpeed: 100 ,
+  backspeed:100,
+  loop: true
+};
+var typed = new Typed('.typedd',variety);
